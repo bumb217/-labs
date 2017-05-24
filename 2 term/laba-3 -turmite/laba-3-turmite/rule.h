f83228f94f1db4233170441786173d855ab10da9
Andrey Prigoda <bumb2172@gmail.com>
2017-05-24 21:27:25 +0300

    Update rule.h

--- 2 term/laba-3 -turmite/laba-3-turmite/rule.h
+++ 2 term/laba-3 -turmite/laba-3-turmite/rule.h
@@ -1,15 +1,7 @@
-//
-//  rule.hpp
-//  laba-3(turmite)
-//
-//  Created by Андрей Пригода on 25.04.17.
-//  Copyright © 2017 Андрей Пригода. All rights reserved.
-//
 
-#ifndef rule_h
-#define rule_h
+#ifndef RULE_H
+#define RULE_H
 
-#include <stdio.h>
 #include <fstream>
 using namespace std;
 
@@ -18,31 +10,32 @@ enum DIR {North, East, South, West};
 class Turmite;
 
 class Rule {
-    char cur_state;
-    int cur_col;
-    
-    char new_state;
-    int new_col;
-    
-    DIR direction;
-    
-    friend class Turmite;
-    
+	char cur_state;
+	int cur_col;
+
+	char new_state;
+	int new_col;
+
+	DIR direction;
+
+	friend class Turmite;
+
 public:
-    
-    Rule(pair<char, int>, pair<char, int>, DIR);
-    
-    static Rule* read(ifstream &in) {
-        int b_color, e_color, direction;
-        char b_state, e_state;
-        
-        in >> b_state;
-        in >> b_color >> e_state;
-        in >> e_color >> direction;
-        
-        return new Rule(pair<char, int>(b_state, b_color),
-                        pair<char, int>(e_state, e_color),
-                        (DIR)direction);
-    }
+
+	Rule(pair<char, int>, pair<char, int>, DIR);
+
+	static Rule* read(ifstream &in) {
+		int b_color, e_color, direction;
+		char b_state, e_state;
+
+		in >> b_state;
+		in >> b_color >> e_state;
+		in >> e_color >> direction;
+
+		return new Rule(pair<char, int>(b_state, b_color),
+						pair<char, int>(e_state, e_color),
+						(DIR)direction);
+	}
 };
-#endif /* rule_h */
+
+#endif
